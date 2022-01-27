31b462ba8e737a77d29970bcd62320d2503dd1bd
Ayari <youssef.ayari@suez.com>
2022-01-27 16:51:24 +0100

    first commit

---
+++ users/user.service.js
@@ -0,0 +1,16 @@
+﻿const users = [{ id: 1, username: 'youssef', password: 'test', firstName: 'ayari', lastName: 'Djo7' }];
+
+module.exports = {
+    authenticate,
+    getAll
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+}
+
+
